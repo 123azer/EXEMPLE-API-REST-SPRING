9738ee9ea82c96a49909fb963dc7370456fe1582
BADR-EDDINE <36115366+123azer@users.noreply.github.com>
2018-11-03 18:03:59 +0000

    Update ContactRestService.java

--- src/main/java/com/example/demo/web/ContactRestService.java
+++ src/main/java/com/example/demo/web/ContactRestService.java
@@ -23,7 +23,7 @@ private ContactRepository contactRepository;
 
 @RequestMapping(value="/MSG",method=RequestMethod.GET)
 public String getContact(){
-	return "hello";
+	return "hello2";
 }
 	
 @RequestMapping(value="/contact",method=RequestMethod.GET)
